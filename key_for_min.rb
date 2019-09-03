68b62d7fc320056a1143bf9bca9d49dc6c16cf11
Bruce Ruffin <bruffin1221@gmail.com>
2019-09-03 18:10:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,29 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  small=[]
+  name_hash.each do |key, value|
+    small<<value
+  end
+  if small[2]<small[0] && small[2]<small[0]
+    :adam
+  elsif small[0]<small[2] && small[0]<small[1]
+    :blake
+  elsif small=[]
+    nil
+  end
+end
+
+hash = {}
+small=[]
+hash.each do |key, value|
+  small<<value
+end
+if small[2]<small[0] && small[2]<small[0]
+  :adam
+elsif small[0]<small[2] && small[0]<small[1]
+  :blake
 
 end
+
+small=
